92a19c1afc75a6496a8229fdb0d6afea827e1d25
Sebastian <sebastianelias.dg@outlook.com>
2024-11-01 23:18:55 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MenuSmart',
+  webDir: 'www'
+};
+
+export default config;
